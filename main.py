71005c3f4508d16b56781c5261a39925156dfe3a
samjhanaluitel <yashcalid@yashs-air.attlocal.net>
2022-11-13 13:30:42 -0800

    add python practice

---
+++ main.py
@@ -0,0 +1,13 @@
+message = 'hello world'
+message1 = "boby's WORLD"
+print(message.replace('hello','namaste'))
+print(message1.lower())
+print(message.count('l'))
+print(message.upper())
+nessage2 = """ hello 
+Nepal"""
+print(nessage2)
+print(message)
+print(message1)
+print(len(message))
+print(message[0])
